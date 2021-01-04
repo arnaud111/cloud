2f96489aeb0d67c48fd5dd65fbf295b68d1af94b
Arnaud <arnaudjourdain111@gmail.com>
2021-01-04 13:26:21 +0100

    4commit

--- pdf.php
+++ pdf.php
@@ -21,7 +21,7 @@
   <a href="users.pdf">download pdf</a>
 
 <?php
-  $ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=' . getenv('WEBTOPDF_API_KEY') . '&content=https://projet-cloud-arnaud-erwan.herokuapp.com/liste.php&html_width=1024&margin=10&title=ListeUser');
+  $ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=' . getenv('WEBTOPDF_API_KEY') . '&content=https://myasso.herokuapp.com/liste.php&html_width=1024&margin=10&title=ListeUser');
 
   $test = curl_exec($ch);
   curl_close($ch);
