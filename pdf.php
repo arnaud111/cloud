41c45d6dca147ac66db06dd2d10bf27dca3e3767
Arnaud <arnaudjourdain111@gmail.com>
2021-01-04 12:57:08 +0100

    4commit

--- pdf.php
+++ pdf.php
@@ -1,11 +1,16 @@
 <?php
 
   require_once('vendor/autoload.php');
+  echo"ok";
   use \PDFShift\PDFShift;
+  echo"ok";
   PDFShift::setApiKey(getenv('PDFSHIFT_API_KEY'));
+  echo"ok";
 
   $data = file_get_content('liste.php');
+  echo"ok";
   PDFShift::convertTo($data, null, 'result.pdf');
+  echo"ok";
 
 
   /*$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=' . getenv('WEBTOPDF_API_KEY') . '&content=https://projet-cloud-arnaud-erwan.herokuapp.com/liste.php&html_width=1024&margin=10&title=ListeUser');
