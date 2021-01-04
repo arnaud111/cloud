7a769c7775aeabcfa40a2aa951472c681c46114d
Arnaud <arnaudjourdain111@gmail.com>
2021-01-04 12:28:00 +0100

    4commit

--- conf.inc.php
+++ conf.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DBHOST", "ec2-54-73-90-195.eu-west-1.compute.amazonaws.com:5432");
+define("DBHOST", "ec2-54-73-90-195.eu-west-1.compute.amazonaws.com");
 define("DBUSER", "lfwueiqiqninhr");
 define("DBPWD", "6f7d32e3352fe949aa8c63d94418af1e2e31297e72683240f58d09cb6a97df0a");
 define("DBNAME", "d9g730mabif3gp");
