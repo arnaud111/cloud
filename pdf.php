e843ca30549cc130466bf37390bc5d1018adb37e
Arnaud <arnaudjourdain111@gmail.com>
2021-01-04 13:11:09 +0100

    4commit

--- pdf.php
+++ pdf.php
@@ -1,25 +1,21 @@
 <?php
   $curl = curl_init();
-  echo "ok";
 
   curl_setopt_array($curl, array(
     CURLOPT_URL => "https://api.pdfshift.io/v3/convert/pdf",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => json_encode(array(
-        "source" => "list.php",
+        "source" => "liste.php",
         "landscape" => false,
         "use_print" => false
     )),
     CURLOPT_HTTPHEADER => array('Content-Type:application/json'),
     CURLOPT_USERPWD => 'api:31ef6864d610413ca62695af726b197b'
   ));
-  echo "ok";
 
   $response = curl_exec($curl);
-  echo "ok";
   file_put_contents('test.pdf', $response);
-  echo "ok";
 ?>
 
 <a href="test.pdf" download>download pdf</a>
