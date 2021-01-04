a4175be2cd0ed6a54fb15508ff371801ee9bc815
Arnaud <arnaudjourdain111@gmail.com>
2021-01-04 13:16:02 +0100

    4commit

--- liste.php
+++ liste.php
@@ -15,37 +15,8 @@
     <form method="post" action="pdf.php">
       <input type='submit' value='Export PDF'>
     </form>
-    <?php
-      $curl = curl_init();
-
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.pdfshift.io/v3/convert/pdf",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => json_encode(array(
-            "source" => "liste.php",
-            "landscape" => false,
-            "use_print" => false
-        )),
-        CURLOPT_HTTPHEADER => array('Content-Type:application/json'),
-        CURLOPT_USERPWD => 'api:31ef6864d610413ca62695af726b197b'
-      ));
-
-      $response = curl_exec($curl);
-      file_put_contents('users.pdf', $response);
-    ?>
     <a href="users.pdf" download>download pdf</a>
     <table>
-      <?php
-
-        $connect = connectDB();
-
-        $listUser = $connect->query("SELECT email FROM USERS");
-        while($user = $listUser->fetch()){
-          echo "<tr><td>".$user["email"]."</td></tr>";
-        }
-
-      ?>
     </table>
   </body>
 
